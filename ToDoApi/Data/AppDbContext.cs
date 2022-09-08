06781adf45db3b5d5250088425a142184ef027fb
MageeMoose <57415911+MageeMoose@users.noreply.github.com>
2022-09-08 23:14:08 +0200

    Add project files.

---
+++ ToDoApi/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace ToDoApi.Data
+{
+    public class AppDbContext :DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDo> ToDos => Set<ToDo>();
+
+    }
+}
