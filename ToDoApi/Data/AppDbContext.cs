646fd80555daacd71fd430d7d40f627d84bf3598
MageeMoose <57415911+MageeMoose@users.noreply.github.com>
2022-09-09 06:36:13 +0200

    Update AppDbContext.cs

--- ToDoApi/Data/AppDbContext.cs
+++ ToDoApi/Data/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace ToDoApi.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
+           
         }
 
         public DbSet<ToDo> ToDos => Set<ToDo>();
