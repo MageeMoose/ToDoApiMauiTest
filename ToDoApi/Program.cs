64770e2ec8c24f6bdf208c9d15d5d4ac5e1c72f6
MageeMoose <57415911+MageeMoose@users.noreply.github.com>
2022-09-21 20:26:59 +0200

    Created a sqliteDb and added 2 endpoints

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,11 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
+using ToDoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
+
+app.MapGet("api/todo", async (AppDbContext context) =>
+{
+    var items = await context.ToDos.ToListAsync();
+
+    return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+
+    await context.SaveChangesAsync();
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
+
 
 //app.UseHttpsRedirection();
 app.Run(); 
